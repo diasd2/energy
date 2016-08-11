af7d39b6ee45d705cf87e4aa9a1f1f767f0e8265
Daciano Dias <Daciano Dias>
2016-08-11 15:46:15 +0100

    Changes to DB access

--- api/config.php
+++ api/config.php
@@ -2,9 +2,9 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'guitaram_login');
+define('DB_PASSWORD', 'aL+0-L274t)n{+ks}%');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'angularcode');
+define('DB_NAME', 'guitaram_energy');
 
 ?>
